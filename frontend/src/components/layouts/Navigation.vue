93c08b2e3de0941b2c0c98b11e994a5a0659a041
Sebastian Ibarra <xebaelvemgador@gmail.com>
2020-09-12 20:42:05 -0300

    Se separaron los layouts y las rutas
    Se separaron las rutas por rol para no tener un archivo gigantesco, ademas tambien se realizo con el los layout

--- frontend/src/components/layouts/Navigation.vue
+++ frontend/src/components/layouts/Navigation.vue
@@ -9,70 +9,25 @@
             Home
             </router-link>
         </li>
-        <template v-if="user.role=='admin'">
-            <li>
-                <router-link
-                :to="{
-                    name:'admin-dashboard'
-                    }"
-                    >
-                    Dashboard
-                    </router-link>
-            </li>
-             <li>
-                <a href="#" @click.prevent="logout">
-                    Logout
-                </a>
-            </li>
-        </template>
-        <template v-else-if="user.role=='hotel'">
-            <li>
-                <router-link
-                :to="{
-                    name:'hotel-dashboard'
-                    }"
-                    >
-                    Dashboard
-                    </router-link>
-            </li>
-             <li>
-                <a href="#" @click.prevent="logout">
-                    Logout
-                </a>
-            </li>
-        </template>
-        <template v-else-if="user.role=='tour'">
-            <li>
-                <router-link
-                :to="{
-                    name:'tour-dashboard'
-                    }"
-                    >
-                    Dashboard
-                    </router-link>
-            </li>
-             <li>
-                <a href="#" @click.prevent="logout">
-                    Logout
-                </a>
-            </li>
-        </template>
-        <template v-else-if="user.role=='user'">
-            <li>
-                <router-link
-                :to="{
-                    name:'user-dashboard'
-                    }"
-                    >
-                    Dashboard
-                    </router-link>
-            </li>
-             <li>
-                <a href="#" @click.prevent="logout">
-                    Logout
-                </a>
-            </li>
-        </template>
+        <template v-if="user.role!=null">
+            <template v-if="user.role=='admin'">
+                <NavigationAdmin/>
+            </template>
+            <template v-else-if="user.role=='hotel'">
+                <NavigationHotel/>
+            </template>
+            <template v-else-if="user.role=='tour'">
+                <NavigationTour/>
+            </template>
+            <template v-else-if="user.role=='user'">
+                <NavigationUser/>
+            </template>
+                <li>
+                    <a href="#" @click.prevent="logout">
+                        Logout
+                    </a>
+                </li>
+        </template>   
         <template v-else>
             <li>
                 <router-link
@@ -89,7 +44,10 @@
 
 <script>
     import {mapGetters, mapActions} from 'vuex'
-
+    import NavigationAdmin from '@/components/layouts/Admin'
+    import NavigationHotel from '@/components/layouts/Hotel'
+    import NavigationTour from '@/components/layouts/Tour'
+    import NavigationUser from '@/components/layouts/User'
     export default {
         computed: {
             ...mapGetters({
@@ -102,7 +60,6 @@
             ...mapActions({
                 logoutAction: 'auth/logout'
             }),
-        
             logout (){
                 this.logoutAction().then(() => {
                     this.$router.replace({
@@ -110,7 +67,13 @@
                     })
                 })
             }
-        }
+        },
+        components: {
+            NavigationAdmin,
+            NavigationHotel,
+            NavigationTour,
+            NavigationUser
+        },
     }
     
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,103 +1,26 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import store from '@/store'
+import VueRouter from 'vue-router';
+
+import { PublicRoutes } from './public';
+import { AdminRoutes } from './admin';
+import { HotelRoutes } from './hotel';
+import { TourRoutes } from './tour';
+import { UserRoutes } from './user';
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import('../views/Home.vue')
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../components/auth/Login.vue')
-  },
-  //route the admin
-  {
-    path: '/admin/dashboard',
-    name: 'admin-dashboard',
-    component: () => import( '../views/admin/Dashboard.vue'),
-    beforeEnter: (to, from, next) => {
-      if (!store.getters['auth/authenticated']) {
-        return next({
-          name: 'login'
-        })
-      }
-      if(store.getters['auth/user'].role!="admin"){
-        return next({
-          name: 'home'
-        })
-      }
-      next()
-    }
-  },
-  //route the hotel
-  {
-    path: '/hotel/dashboard',
-    name: 'hotel-dashboard',
-    component: () => import( '../views/hotel/Dashboard.vue'),
-    beforeEnter: (to, from, next) => {
-      if (!store.getters['auth/authenticated']) {
-        return next({
-          name: 'login'
-        })
-      }
-      if(store.getters['auth/user'].role!="hotel"){
-        return next({
-          name: 'home'
-        })
-      }
-      next()
-    }
-  },
-  //route the tour
-  {
-    path: '/tour/dashboard',
-    name: 'tour-dashboard',
-    component: () => import( '../views/tour/Dashboard.vue'),
-    beforeEnter: (to, from, next) => {
-      if (!store.getters['auth/authenticated']) {
-        return next({
-          name: 'login'
-        })
-      }
-      if(store.getters['auth/user'].role!="tour"){
-        return next({
-          name: 'home'
-        })
-      }
-      next()
-    }
-  },
-  //route the user
-  {
-    path: '/user/dashboard',
-    name: 'user-dashboard',
-    component: () => import( '../views/user/Dashboard.vue'),
-    beforeEnter: (to, from, next) => {
-      if (!store.getters['auth/authenticated']) {
-        return next({
-          name: 'login'
-        })
-      }
-      if(store.getters['auth/user'].role!="user"){
-        return next({
-          name: 'home'
-        })
-      }
-      next()
-    }
-  },
-]
+  ...PublicRoutes,
+  ...AdminRoutes,
+  ...HotelRoutes,
+  ...TourRoutes,
+  ...UserRoutes,
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
-
 
 export default router
